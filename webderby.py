0b6faa167fcc6d439a0e5a5eaa2ea1bab3115f61
jrsewing1 <jrsewing1@hotmail.com>
2020-07-18 16:04:25 -0600

    db updates

--- webderby.py
+++ webderby.py
@@ -1,8 +1,30 @@
+from datetime import datetime
 from flask import Flask, escape, request, render_template, flash, redirect, url_for
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '034e6e1b7628c772357906be031cdbd8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class Driver(db.Model):
+		id = db.Column(db.Integer, primary_key=True)
+		drivername = db.Column(db.String(30), unique=True, nullable=False)
+		email = db.Column(db.String(120), unique=True, nullable=False)
+		immage_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+		password = db.Column(db.String(60), nullable=False)
+
+		def __repr__(self):
+			return f"Driver('{self.drivername}', '{self.email}', '{self.image_file}')"
+
+class Cars(db.Model):
+		id = db.Column(db.Integer, primary_key=True)
+		car_name = db.Column(db.String(30), unique=True, nullable=False)
+		weight = db.Column(db.String(30), unique=True, nullable=False)
+
+		def __repr__(self):
+			return f"Driver('{self.car_name}', '{self.weight})"
 
 cars = [
 	{
@@ -20,8 +42,6 @@ cars = [
 		'driver': 'Anthony Lalande',
 		'lane': '3'
 	}	
-
-
 ]
 
 
